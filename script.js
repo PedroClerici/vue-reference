8fe5ef6474e6f2b6519b276790c5df50242965d2
Pedro Clerici <pedro.clerici@tutamail.com>
2023-03-01 22:14:52 -0300

    Added vue to the project

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!")
+const dayOfTheWeek = new Date().toLocaleDateString("en-US", { weekday: 'long'})
+
+const App = {
+  data() {
+    return {
+      greeting: `Hello, today is ${dayOfTheWeek}.`,
+      isItTrue: true,
+      names: ["Francis", "Maria", "Steve", "Scott"],
+      car: {
+        brand: "Honda",
+        model: "Civic",
+      },
+      number: 15,
+      isDevisableByThree(number) {
+        return number % 3 == 0
+      },
+    }
+  }
+}
+
+Vue.createApp(App).mount("#app")

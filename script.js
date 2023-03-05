34c8801e75c7252d09d7903e450d13de705c915a
Pedro Clerici <pedro.clerici@tutamail.com>
2023-03-04 21:15:33 -0300

    Added events

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const dayOfTheWeek = new Date().toLocaleDateString("en-US", { weekday: 'long'})
-
 const App = {
   data() {
     return {
-      greeting: `Hello, today is ${dayOfTheWeek}.`,
-      isItTrue: true,
-      names: ["Francis", "Maria", "Steve", "Scott"],
-      car: {
-        brand: "Honda",
-        model: "Civic",
-      },
-      number: 15,
-      isDevisableByThree(number) {
-        return number % 3 == 0
-      },
+      name: "Scott",
+      x: 0,
+      y: 0,
     }
+  },
+  methods: {
+    changeName() {
+      this.name === "Mario" ? this.name = "Scott" : this.name = "Mario"
+    },
+    getCoordinates(event) {
+      this.x = event.offsetX
+      this.y = event.offsetY
+    },
   }
 }
 

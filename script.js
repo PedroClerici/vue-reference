48bc3dc4da40a6f281581879e98914bc96bc006e
Pedro Clerici <pedro.clerici@tutamail.com>
2023-03-06 17:16:18 -0300

    Added lol

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const dayOfTheWeek = new Date().toLocaleDateString("en-US", { weekday: 'long'})
-
 const App = {
   data() {
     return {
-      greeting: `Hello, today is ${dayOfTheWeek}.`,
-      isItTrue: true,
-      names: ["Francis", "Maria", "Steve", "Scott"],
-      car: {
-        brand: "Honda",
-        model: "Civic",
-      },
-      number: 15,
-      isDevisableByThree(number) {
-        return number % 3 == 0
-      },
     }
   }
 }

c5b115a05782491ebfc307710115ae8495809877
Pedro Clerici <pedro.clerici@tutamail.com>
2023-03-06 23:23:06 -0300

    Made vue handle styles

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 const App = {
   data() {
     return {
+      isRed: false,
+      isLarge: false,
+      colors: {
+        red: false,
+        green: false,
+        blue: false,
+      },
+      newStyle: {
+        display: "flex",
+        alignItems: "center",
+        fontWeight: "bold",
+        textAlign: "center",
+        border: "5px solid black",
+      }
+    }
+  },
+  methods: {
+    changeColor() {
+      this.isRed = !this.isRed
+    },
+    changeSize() {
+      this.isLarge = !this.isLarge
+    },
+    // Turns a random color form colors to true
+    randomColor() {
+      for(color in this.colors) {
+        this.colors[color] = false
+      }
+
+      const randomColorIndex = Math.floor(
+        Math.random() * Object.keys(this.colors).length
+      )
+
+      const randomColorKey = Object.keys(this.colors)[randomColorIndex]
+
+      this.colors[randomColorKey] = true
     }
   }
 }
